1b5bbbd8742da8b27943706776d90cae6fdb9b2b
shuhad zaman <shuhad zaman>
2015-11-01 17:18:20 +0600

    top menu edit

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -21,7 +21,7 @@ AppAsset::register($this);
 <body ng-controller="MainController">
 <?php $this->beginBody() ?>
 <div class="wrap">
-    <nav class="navbar-inverse navbar-fixed-top navbar" role="navigation" bs-navbar>
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inner" role="navigation" bs-navbar>
         <div class="container">
             <div class="navbar-header">
                 <button ng-init="navCollapsed = true" ng-click="navCollapsed = !navCollapsed" type="button" class="navbar-toggle">
@@ -33,18 +33,42 @@ AppAsset::register($this);
             </div>
             <div ng-class="!navCollapsed && 'in'" ng-click="navCollapsed=true" class="collapse navbar-collapse" >
                 <ul class="navbar-nav navbar-right nav">
-                    <li data-match-route="/$">
-                        <a href="#/">Home</a>
+                    <li data-match-route="/dashboard" ng-show="loggedIn()" class="ng-hide">
+                        <a href="#/dashboard">Dashboard</a>
                     </li>
+                     <li class="dropdown" uib-dropdown>
+                    <a role="button" class="dropdown-toggle" uib-dropdown-toggle>
+                        Books <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                       <li data-match-route="book/create">
+                        	<a href="#/book/create">Books</a>
+                    	</li>
+                        <li data-match-route="book/index">
+                        	<a href="#/book/index">Manage Books</a>
+                    	</li>
+                    </ul>
+                </li>
+                    <li class="dropdown" uib-dropdown>
+                    <a role="button" class="dropdown-toggle" uib-dropdown-toggle>
+                        Book Category <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                       <li data-match-route="book/create_cat">
+                        	<a href="#/book/create_cat">Book Category</a>
+                    	</li>
+                        <li data-match-route="book/categories">
+                        	<a href="#/book/categories">Manage Books Category</a>
+                    	</li>
+                    </ul>
+                </li>
                     <li data-match-route="/about">
                         <a href="#/about">About</a>
                     </li>
                     <li data-match-route="/contact">
                         <a href="#/contact">Contact</a>
                     </li>
-                    <li data-match-route="/dashboard" ng-show="loggedIn()" class="ng-hide">
-                        <a href="#/dashboard">Dashboard</a>
-                    </li>
+                    
                     <li ng-class="{active:isActive('/logout')}" ng-show="loggedIn()" ng-click="logout()"  class="ng-hide">
                         <a href="">Logout</a>
                     </li>
